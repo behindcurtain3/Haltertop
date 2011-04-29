afd971a063bba780b5ef7bf2b3d3d05cf1ff5a2f
behindcurtain3 <behindcurtain3@gmail.com>
2011-04-28 23:25:30 -0700

    Added users & sessions

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,99 @@
+class UsersController < ApplicationController
+  # before_filters are run before anything else is done on the controller
+  # Authenticate the user
+  before_filter :authenticate,
+    :except => [ :show, :new, :create ]
+
+  # Check for the correct user
+  before_filter :correct_user,
+    :only => [ :edit, :update ]
+
+  # Check for admin
+  before_filter :admin_user,
+    :only => [ :destroy ]
+
+  # Check if the user is already signed in
+  before_filter :already_in,
+    :only => [ :new, :create ]
+
+  # GET /users
+  def index
+    @title = "Users"
+    @users = User.all
+  end
+
+  # GET /users/#{id}
+  def show
+    @user = User.find(params[:id])
+    @title = @user.name
+  end
+
+  # GET /users/new
+  def new
+    @title = "Sign up"
+    @user = User.new
+  end
+
+  # GET /users/#{id}/edit
+  def edit
+    @user = User.find(params[:id])
+    @title = "Edit"
+  end
+
+  # POST /users
+  def create
+    @user = User.new(params[:user])
+
+    # Attempt to save the new user
+    if @user.save
+      # if successful, sign them in & welcome them
+      sign_in @user
+      flash[:success] = "Welcome!"
+      redirect_to @user
+    else
+      # if not successful, blank the passwords so they aren't sent back
+      @user.password = ''
+      @user.password_confirmation = ''
+      render :action => "new"
+    end
+  end
+
+  # PUT /users/#{id}
+  def update
+    @user = User.find(params[:id])
+
+    # Attempt to update attributes
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Settings saved."
+    else
+      # if not successful re-render the edit page
+      @title = "Edit"
+      render :action => "edit"
+    end
+  end
+
+  # DELETE /users/#{id}
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User terminated"
+    redirect_to users_path
+  end
+
+  # Private methods
+  private
+    def correct_user
+      @user = User.find(params[:id])
+
+      # Redirect to home if they aren't the current user
+      redirect_to root_path unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to root_path unless current_user.admin?
+    end
+
+    def already_in
+      redirect_to root_path if signed_in?
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Haltertop::Application.routes.draw do
+  resources :users
+  resources :sessions,
+    :only => [ :new, :create, :destroy ]
+
   root :to => "pages#home"
 
-  match 'news'          => "pages#news"
-  match 'getstarted'    => "pages#getstarted"
-	match 'signin'        => "sessions#new"
-  match 'about'         => "pages#about"
-  match 'contact'       => "pages#contact"
+  match '/news'          => "pages#news"
+  match '/getstarted'    => "pages#getstarted"
+  match '/about'         => "pages#about"
+  match '/contact'       => "pages#contact"
+
+  match '/signin'        => "sessions#new"
+  match '/signout'       => "session#destroy"
+  match '/signup'        => "users#new"
 end

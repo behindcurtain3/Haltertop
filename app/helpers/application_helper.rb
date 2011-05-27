3cf7fb240ebdc1ab46f64ca37f1a82c6b7ba7a76
Justin <behindcurtain3@gmail.com>
2011-05-27 01:05:14 -0700

    Changed references to Haltertop to SocialChess

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   # Return a title on a per-page basis.
   def title
-    base_title = "Haltertop"
+    base_title = "SocialChess"
     if @title.nil?
       base_title
     else

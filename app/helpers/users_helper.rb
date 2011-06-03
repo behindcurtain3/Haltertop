142f1cb0485ae1e8bfc262338ea4ab6a77f848d0
Justin <behindcurtain3@gmail.com>
2011-06-03 04:22:35 -0700

    Add user box partial & css, removed old file, swapped black & white sides on match list to reflect the way results are written (1-0 for white win or 0-1 for black win)

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,13 +1,13 @@
 module UsersHelper
 
-	def image_for(user, options = { :size => 48 })
+	def image_for(user, classes = "sm_round", options = { :size => 48 })
 		if user.nil?
-			gravatar_image_tag("", :alt => "No Icon", :class => "sm_round", :gravatar => options)
+			gravatar_image_tag("", :alt => "No Icon", :class => classes, :gravatar => options)
 		elsif user.facebook
-			image_tag user.facebook.get_picture(user.fbid), :alt => user.name, :size => "48x48" , :class => "sm_round"
+			image_tag user.facebook.get_picture(user.fbid), :alt => user.name, :size => "48x48" , :class => classes
 		else
 			gravatar_image_tag(user.email.downcase, :alt => user.name,
-                                            :class => "sm_round",
+                                            :class => classes,
                                             :gravatar => options)
 		end
 

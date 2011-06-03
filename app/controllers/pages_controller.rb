e45fb6cfb9b08393d7de4466bc8521cf3c72359d
Justin <behindcurtain3@gmail.com>
2011-06-02 22:32:34 -0700

    fixed bug in page controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,10 @@ class PagesController < ApplicationController
   def home
 		@title = "Home"
 		@user = User.new
+		@friends = []
 		if signed_in? && facebook_signed
 			@fb_friends = facebook_signed.get_connections("me", "friends")
-			@friends = []
+			
 			@fb_friends.each do | friend |
 				user = User.find_by_fbid(friend['id'])
 				if user
